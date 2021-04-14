778be01d42f66ed39f6ac08abb9ac2d0efd53d47
Joel Sandman <joel@sandman.nu>
2021-04-14 13:18:19 +0200

    Added CSV file output similar to that of the gRPC interceptor

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+    "os"
     "strconv"
     "time"
 
@@ -15,17 +16,43 @@ import (
     "github.com/patrickmn/go-cache"
 )
 
+const (
+	csvFileName             = "data.csv"
+	memoryUsageCsvFileName  = "memory-data.csv"
+)
+
 func main() {
-	port := ":1234"
-	u, err := url.Parse("http://localhost:8080")
+    expiration, err := strconv.Atoi(os.Getenv("TTL"))
 	if err != nil {
-		log.Fatalf("Could not parse downstream url: %s", u)
+		log.Fatalf("TTL cannot be parsed as integer")
+	}
+
+    port, err := strconv.Atoi(os.Getenv("PROXY_LISTEN_PORT"))
+	if err != nil {
+		log.Fatalf("PROXY_LISTEN_PORT cannot be parsed as integer")
+	}
+
+	path, err := url.Parse(fmt.Sprintf("http://%s", os.Getenv("FRONTEND_ADDR")))
+	if err != nil {
+		log.Fatalf("Could not parse downstream url: %s", path)
 	}
 
     proxyCache := *cache.New(10*time.Second, 60*time.Second)
-    expiration := 60000
 
-    go memoryUsageStatus(proxyCache)
+    memoryUsageCsvFile, err := os.Create(memoryUsageCsvFileName)
+	if err != nil {
+		log.Fatalf("Could not open CSV file (%s) for writing", memoryUsageCsvFile)
+	}
+	defer memoryUsageCsvFile.Close()
+    go memoryUsageStatus(proxyCache, log.New(memoryUsageCsvFile, "", 0))
+
+    csvFile, err := os.Create(csvFileName)
+	if err != nil {
+		log.Fatalf("Could not open CSV file (%s) for writing", csvFileName)
+	}
+	defer csvFile.Close()
+    csvLog := log.New(csvFile, "", 0)
+    csvLog.Printf("timestamp,source,info,size,url(hash)\n")
 
     handler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
         reqBody, err := ioutil.ReadAll(req.Body)
@@ -36,31 +63,42 @@ func main() {
         if err != nil {
             log.Printf("Error closing req body")
         }
-        reqURL := req.URL.String()
-        hash := hashcode.Strings([]string{reqURL, string(reqBody)})
-        proxy := httputil.NewSingleHostReverseProxy(u)
+        reqUrl := req.URL.String()
+        reqSize := len(reqBody)
+
+        hash := hashcode.Strings([]string{reqUrl, string(reqBody)})
+        proxy := httputil.NewSingleHostReverseProxy(path)
         proxy.ModifyResponse = func(res *http.Response) error {
             resBody, err := ioutil.ReadAll(res.Body)
             if err != nil {
-                return  err
+                return err
             }
             err = res.Body.Close()
             if err != nil {
                 return err
             }
             if cachedValue, found := proxyCache.Get(hash); found {
-                log.Printf("Found in cache!")
+                // log.Printf("Found in cache!")
                 cachedValueString := fmt.Sprintf("%v", cachedValue)
-                // match := "stale"
+
+                resSize := len(resBody)
+                totSize := reqSize + resSize
+
+                match := "stale"
                 if string(resBody) == cachedValueString {
-                    // match = "fresh"
+                    match = "fresh"
                     log.Printf("Fresh data in cache")
                 } else {
                     log.Printf("Stale data in cache")
                 }
+                csvLog.Printf("%d,cache,%s,%d,%s(%s)\n", time.Now().UnixNano(), match, totSize, reqUrl, hash)
             } else {
-                log.Printf("NOT found in cache!")
+                // log.Printf("NOT found in cache!")
+                resSize := len(resBody)
+                totSize := reqSize + resSize
+
                 proxyCache.Set(hash, string(resBody), time.Duration(expiration)*time.Millisecond)
+                csvLog.Printf("%d,downstream,,%d,%s(%s)\n", time.Now().UnixNano(), totSize, reqUrl, hash)
             }
         
             newResbody := ioutil.NopCloser(bytes.NewReader(resBody))
@@ -76,11 +114,11 @@ func main() {
         proxy.ServeHTTP(w, req)
     })
 
-    log.Fatal(http.ListenAndServe(port, handler))
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handler))
 }
 
-func memoryUsageStatus(proxyCache cache.Cache) {
-	// csvLog.Printf("timestamp,items,bytes")
+func memoryUsageStatus(proxyCache cache.Cache, csvLog *log.Logger) {
+	csvLog.Printf("timestamp,items,bytes")
 	for {
 		time.Sleep(15 * time.Second)
 		proxyCache.DeleteExpired()
@@ -95,6 +133,6 @@ func memoryUsageStatus(proxyCache cache.Cache) {
 		size := buf.Len()
 		log.Printf("Size of cache (bytes): %d", size)
 
-		// csvLog.Printf("%d,%d,%d", time.Now().UnixNano(), items, size)
+		csvLog.Printf("%d,%d,%d", time.Now().UnixNano(), items, size)
 	}
 }
